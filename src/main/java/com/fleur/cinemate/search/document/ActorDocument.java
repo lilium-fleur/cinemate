1e43ae9c05c69593c38bddd8b10576acd13e8520
haruxime <haruxime01@gmail.com>
2025-03-24 23:38:28 +0400

    Create elastic search index for actors with mapping

---
+++ src/main/java/com/fleur/cinemate/search/document/ActorDocument.java
@@ -0,0 +1,23 @@
+package com.fleur.cinemate.search.document;
+
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Builder
+@Document(indexName = "actors")
+public class ActorDocument {
+
+    @Id
+    private Long id;
+
+    @Field(type = FieldType.Text, analyzer = "english")
+    private String name;
+}
