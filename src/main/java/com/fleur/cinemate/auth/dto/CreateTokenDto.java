fc7267a2d6c585e360539e7d489c633ff03c6952
haruxime <haruxime01@gmail.com>
2025-03-24 23:54:14 +0400

    Create rating controller

--- src/main/java/com/fleur/cinemate/auth/dto/CreateTokenDto.java
+++ src/main/java/com/fleur/cinemate/auth/dto/CreateTokenDto.java
@@ -1,7 +1,7 @@
 package com.fleur.cinemate.auth.dto;
 
 import com.fleur.cinemate.user.User;
-import com.fleur.cinemate.user.usersession.UserSession;
+import com.fleur.cinemate.user.session.UserSession;
 import lombok.Builder;
 
 @Builder
--- src/main/java/com/fleur/cinemate/core/rating/RatingController.java
+++ src/main/java/com/fleur/cinemate/core/rating/RatingController.java
@@ -1,9 +1,9 @@
-package com.fleur.cinemate.core.raiting;
+package com.fleur.cinemate.core.rating;
 
 
-import com.fleur.cinemate.core.raiting.dto.CreateRatingDto;
-import com.fleur.cinemate.core.raiting.dto.RatingDto;
-import com.fleur.cinemate.core.raiting.dto.UpdateRatingDto;
+import com.fleur.cinemate.core.rating.dto.CreateRatingDto;
+import com.fleur.cinemate.core.rating.dto.RatingDto;
+import com.fleur.cinemate.core.rating.dto.UpdateRatingDto;
 import com.fleur.cinemate.user.User;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
