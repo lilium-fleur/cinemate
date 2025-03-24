732825b4de9f2eace08685ee67dde5c06fc0811a
haruxime <haruxime01@gmail.com>
2025-03-24 23:37:53 +0400

    Create elastic search index for films with mapping

---
+++ src/main/java/com/fleur/cinemate/search/document/FilmDocument.java
@@ -0,0 +1,47 @@
+package com.fleur.cinemate.search.document;
+
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Builder
+@Document(indexName = "films")
+public class FilmDocument {
+
+    @Id
+    private Long id;
+
+    @Field(type = FieldType.Text, analyzer = "english")
+    private String title;
+
+    @Field(type = FieldType.Text, analyzer = "english")
+    private String description;
+
+    @Field(type = FieldType.Integer)
+    private Integer releaseYear;
+
+    @Field(type = FieldType.Keyword, normalizer = "lowercase")
+    @Builder.Default
+    private Set<String> genres = new HashSet<>();
+
+    @Field(type = FieldType.Keyword, normalizer = "lowercase")
+    @Builder.Default
+    private Set<String> actors = new HashSet<>();
+
+    @Field(type = FieldType.Text, name = "genres_search", analyzer = "english")
+    private Set<String> genresSearch;
+
+    @Field(type = FieldType.Text, name = "actors_search", analyzer = "english")
+    private Set<String> actorsSearch;
+
+}
---
+++ src/main/java/com/fleur/cinemate/search/repository/FilmDocumentRepository.java
@@ -0,0 +1,13 @@
+package com.fleur.cinemate.search.repository;
+
+import com.fleur.cinemate.search.document.FilmDocument;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping
+public interface FilmDocumentRepository extends ElasticsearchRepository<FilmDocument, Long> {
+
+    Page<FilmDocument> findByTitleContaining(String title, Pageable pageable);
+}
